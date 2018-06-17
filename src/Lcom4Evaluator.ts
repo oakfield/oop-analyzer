6824cb3c953c7340cd8adc3fbacf17dddb90c019
Michael Smith <michael.christian.smith@gmail.com>
2018-06-17 14:58:51 -0600

    added very basic command line interface

--- src/Lcom4Evaluator.ts
+++ src/Lcom4Evaluator.ts
@@ -3,7 +3,7 @@ import Lcom4Converter from './Lcom4Converter';
 export default class Lcom4Evaluator<T> implements IEvaluator<T> {
 	constructor(private _converter: Lcom4Converter) { }
 
-	evaluate(toEvaluate: { variables: string[], methods: Method[] }): number {
+	evaluate(toEvaluate: ClassModel): number {
 		return this._converter.convert(toEvaluate).connectedComponents.length;
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,22 @@
 import Lcom4Converter from "./Lcom4Converter";
 import Lcom4Evaluator from "./Lcom4Evaluator";
 import Lcom4Validator from "./Lcom4Validator";
-import WmcEvaluator from "./WmcEvaluator";
+// import WmcEvaluator from "./WmcEvaluator";
 import Parser from "./Parser";
 
-let testClass = {
-	name: "Test",
-	variables: ["x", "y"],
-	methods: [
-		{
-			name: "a",
-			references: ["b"]
-		},
-		{
-			name: "b",
-			references: ["x"]
-		},
-		{
-			name: "c",
-			references: ["x"]
-		},
-		{
-			name: "d",
-			references: ["y", "e"]
-		},
-		{
-			name: "e",
-			references: []
-		}
-	]
-};
+let args = process.argv.slice(2);
+let testClassString = args[0];
+let testClasses: ClassModel[] = new Parser().parse(testClassString);
 
-console.log(JSON.stringify(new Parser().parse("class Test { constructor() { this.myVariable1 = null; this.myVariable2 = 3; } myMethod() { console.log(this.myVariable1); } }")));
+for (let testClass of testClasses) {
+	let lcom4Converter = new Lcom4Converter();
+	let lcom4Evaluator = new Lcom4Evaluator(lcom4Converter);
+	let lcom4Validator = new Lcom4Validator(lcom4Converter);
 
-let lcom4Converter = new Lcom4Converter();
+	console.log(`Class: ${testClass.name}`);
+	console.log(`LCOM4: ${lcom4Evaluator.evaluate(testClass)}`);
+	console.log(`Corrected: ${JSON.stringify(lcom4Validator.validate(testClass))}`);
+}
 
-let lcom4Evaluator = new Lcom4Evaluator(lcom4Converter);
-// console.log(lcom4Evaluator.evaluate(testClass));
-
-let wmcEvaluator = new WmcEvaluator();
-// console.log(wmcEvaluator.evaluate(testClass));
-
-let lcom4Validator = new Lcom4Validator(lcom4Converter);
+// let wmcEvaluator = new WmcEvaluator();
 // console.log(JSON.stringify(lcom4Validator.validate(testClass)));
