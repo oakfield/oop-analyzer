f799ec77eded1c834e4a47eb2021186279764e12
Michael Smith <michael.christian.smith@gmail.com>
2019-04-07 15:36:40 -0600

    added specs for Node

---
+++ src/app/Node.spec.ts
@@ -0,0 +1,86 @@
+import "mocha";
+
+import Node from "./Node";
+import { expect } from "chai";
+
+describe("Node", () => {
+    describe("constructor", () => {
+        it("initializes instance variables", () => {
+            const data = "test";
+            let node = new Node<string>(data);
+
+            expect(node.data).to.equal(data);
+            expect(node.neighbors).not.to.be.undefined;
+        });
+    });
+
+    describe("*partialDepthFirstSearch", () => {
+        it("searches through one node when the node has no neighbors", () => {
+            let node = new Node<string>("test");
+
+            let searchedNodes: Node<string>[] = [];
+            for (let searchedNode of node.partialDepthFirstSearch()) {
+                searchedNodes.push(searchedNode);
+            }
+
+            expect(searchedNodes.length).to.equal(1);
+            expect(searchedNodes).to.include(node);
+        });
+
+        it("searches through all neighbors given multiple neighbors", () => {
+            let node1 = new Node<string>("test1");
+            let node2 = new Node<string>("test2");
+            let node3 = new Node<string>("test3");
+            let node4 = new Node<string>("test4");
+            let node5 = new Node<string>("test5");
+            let node6 = new Node<string>("test6");
+
+            node1.neighbors.add(node2);
+            node1.neighbors.add(node3);
+            node1.neighbors.add(node4);
+            node1.neighbors.add(node5);
+            node1.neighbors.add(node6);
+
+            let searchedNodes: Node<string>[] = [];
+            for (let searchedNode of node1.partialDepthFirstSearch()) {
+                searchedNodes.push(searchedNode);
+            }
+
+            expect(searchedNodes.length).to.equal(6);
+            expect(searchedNodes).to.include(node1);
+            expect(searchedNodes).to.include(node2);
+            expect(searchedNodes).to.include(node3);
+            expect(searchedNodes).to.include(node4);
+            expect(searchedNodes).to.include(node5);
+            expect(searchedNodes).to.include(node6);
+        });
+
+        it("searches recursively", () => {
+            let node1 = new Node<string>("test1");
+            let node2 = new Node<string>("test2");
+            let node3 = new Node<string>("test3");
+            let node4 = new Node<string>("test4");
+            let node5 = new Node<string>("test5");
+            let node6 = new Node<string>("test6");
+
+            node1.neighbors.add(node2);
+            node2.neighbors.add(node3);
+            node3.neighbors.add(node4);
+            node4.neighbors.add(node5);
+            node5.neighbors.add(node6);
+
+            let searchedNodes: Node<string>[] = [];
+            for (let searchedNode of node1.partialDepthFirstSearch()) {
+                searchedNodes.push(searchedNode);
+            }
+
+            expect(searchedNodes.length).to.equal(6);
+            expect(searchedNodes).to.include(node1);
+            expect(searchedNodes).to.include(node2);
+            expect(searchedNodes).to.include(node3);
+            expect(searchedNodes).to.include(node4);
+            expect(searchedNodes).to.include(node5);
+            expect(searchedNodes).to.include(node6);
+        });
+    });
+});
