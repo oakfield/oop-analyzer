d1005395f9798a04280b30c5d6aa131b1f88ffd5
Michael Smith <michael.christian.smith@gmail.com>
2019-04-12 21:40:04 -0600

    added some more specs for improving lcom1

--- src/integration_specs/JavaScriptFileImproveLcom1.spec.ts
+++ src/integration_specs/JavaScriptFileImproveLcom1.spec.ts
@@ -26,4 +26,82 @@ describe("Transform JavaScript file to improve LCOM1", () => {
 
 		expect(transformedLcom1 <= initialLcom1).to.equal(true);
 	});
+
+	it("does not worsen LCOM1 when given a class with one variable", () => {
+		let javaScript = `class AnotherTest { constructor() { this.testVar = 'foo'; } }`;
+		let javaScriptFile = new JavaScriptFile(javaScript);
+		let converter = new Lcom1Converter();
+		let metric = new Lcom1Metric(converter);
+		let maximalCliqueTransformer = new MaximalCliqueTransformer(converter);
+
+		// Get initial LCOM1.
+		let classModel = javaScriptFile.toClassModelArray()[0];
+		let initialLcom1 = metric.evaluate(classModel);
+
+		// Transform.
+		let transformedClassModel = maximalCliqueTransformer.transform(classModel)[0];
+
+		// Get transformed LCOM1.
+		let transformedLcom1 = metric.evaluate(transformedClassModel);
+
+		expect(transformedLcom1 <= initialLcom1).to.equal(true);
+	});
+
+	it("preserves the content of a class with one variable", () => {
+		const variableName = "testVar";
+		const variableAssignmentSource = `this.${variableName} = 'foo';`;
+		let javaScript = `class AnotherTest { constructor() { ${variableAssignmentSource} } }`;
+		let javaScriptFile = new JavaScriptFile(javaScript);
+		let converter = new Lcom1Converter();
+		let maximalCliqueTransformer = new MaximalCliqueTransformer(converter);
+
+		// Get initial LCOM1.
+		let classModel = javaScriptFile.toClassModelArray()[0];
+		let transformedClassModel = maximalCliqueTransformer.transform(classModel)[0];
+
+		expect(transformedClassModel.variables.length).to.equal(1);
+		expect(transformedClassModel.variables[0].name).to.equal(variableName);
+		expect(transformedClassModel.variables[0].toString()).to.equal(variableAssignmentSource);
+	});
+
+	it("does not worsen LCOM1 when given a class with one variable and one method that doesn't reference the variable", () => {
+		let javaScript = `class AnotherTest { constructor() { this.testVar = 'foo'; } testMethod() { } }`;
+		let javaScriptFile = new JavaScriptFile(javaScript);
+		let converter = new Lcom1Converter();
+		let metric = new Lcom1Metric(converter);
+		let maximalCliqueTransformer = new MaximalCliqueTransformer(converter);
+
+		// Get initial LCOM1.
+		let classModel = javaScriptFile.toClassModelArray()[0];
+		let initialLcom1 = metric.evaluate(classModel);
+
+		// Transform.
+		let transformedClassModel = maximalCliqueTransformer.transform(classModel)[0];
+
+		// Get transformed LCOM1.
+		let transformedLcom1 = metric.evaluate(transformedClassModel);
+
+		expect(transformedLcom1 <= initialLcom1).to.equal(true);
+	});
+
+	it("preserves the content of a class with one variable and one method that doesn't reference the variable", () => {
+		const variableName = "testVar";
+		const variableAssignmentSource = `this.${variableName} = 'foo';`;
+		const methodName = "testMethod";
+		const methodSource = `${methodName}() { }`;
+		let javaScript = `class AnotherTest { constructor() { ${variableAssignmentSource} } ${methodSource} }`;
+		let javaScriptFile = new JavaScriptFile(javaScript);
+		let converter = new Lcom1Converter();
+		let maximalCliqueTransformer = new MaximalCliqueTransformer(converter);
+
+		let classModel = javaScriptFile.toClassModelArray()[0];
+		let transformedClassModel = maximalCliqueTransformer.transform(classModel)[0];
+
+		expect(transformedClassModel.variables.length).to.equal(1);
+		expect(transformedClassModel.variables[0].name).to.equal(variableName);
+		expect(transformedClassModel.variables[0].toString()).to.equal(variableAssignmentSource);
+		expect(transformedClassModel.methods.length).to.equal(1);
+		expect(transformedClassModel.methods[0].name).to.equal(methodName);
+		expect(transformedClassModel.methods[0].toString()).to.equal(methodSource);
+	});
 });
