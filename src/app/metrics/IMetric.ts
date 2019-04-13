8d5d5424f2ef5a897fabe3c6c8ca6cf57cdb50f9
Michael Smith <michael.christian.smith@gmail.com>
2019-04-12 21:51:10 -0600

    added some more missing specs

--- src/app/metrics/IMetric.ts
+++ src/app/metrics/IMetric.ts
@@ -4,5 +4,9 @@ import ClassModel from "../models/ClassModel";
  * The interface shared by metrics.
  */
 export default interface IMetric {
+	/**
+	 * Returns a number measuring the class.
+	 * @param classModel the class to evaluate
+	 */
 	evaluate(classModel: ClassModel): number;
 }
